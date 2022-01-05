e5775e5e584d4d383cce6df57b3ea0d47d646e38
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-01-05 15:56:05 +0800

    coding new one

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 $(function () {
-  let hm_click = false;
-  let clicked = false;
 
-  $("#hm").click(function () {
-    if (hm_click === clicked) {
-      $("#hm_exp").attr("aria-expanded", "true");
-      $(this).attr("aria-expanded", "true");
-      hm_click = true;
-      return;
-    }
-    $("#hm_exp").attr("aria-expanded", "false");
-    $(this).attr("aria-expanded", "false");
-    hm_click = false;
-  });
 })
 
